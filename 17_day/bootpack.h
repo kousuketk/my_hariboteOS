2e5f5f6dec574fbb395a512a723caa1cf61f5071
kousuke takahashi <pamarikomi_t@icloud.com>
2021-03-14 14:06:51 +0900

    modify

--- 17_day/bootpack.h
+++ 17_day/bootpack.h
@@ -36,7 +36,7 @@ struct FIFO32 {
 	int p, q, size, free, flags;
 	struct TASK *task;
 };
-void fifo32_init(struct FIFO32 *fifo, int size, int *buf,  struct TASK *task);
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
 int fifo32_put(struct FIFO32 *fifo, int data);
 int fifo32_get(struct FIFO32 *fifo);
 int fifo32_status(struct FIFO32 *fifo);
