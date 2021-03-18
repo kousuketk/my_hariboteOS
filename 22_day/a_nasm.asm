205d4b61b9ff2647da0e005319b24e6ae2171396
kousuke takahashi <pamarikomi_t@icloud.com>
2021-03-18 18:47:45 +0900

    22day_d

--- 22_day/a_nasm.asm
+++ 22_day/a_nasm.asm
@@ -1,10 +1,19 @@
 GLOBAL	api_putchar
+GLOBAL	api_putstr0
 GLOBAL	api_end
 
 api_putchar:	; void api_putchar(int c);
 		MOV		EDX,1
 		MOV		AL,[ESP+4]		; c
 		INT		0x40
+		RET
+
+api_putstr0:	; void api_putstr0(char *s);
+		PUSH	EBX
+		MOV		EDX,2
+		MOV		EBX,[ESP+8]		; s
+		INT		0x40
+		POP		EBX
 		RET
 
 api_end:	; void api_end(void);
